c6c719a0f06a52192934a969cb8fba2794a3ca3b
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 15:16:13 -0500

    Add: Link to card

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import type ICharacter from "../types/character";
 
 type CharacterProps = {
@@ -6,11 +7,13 @@ type CharacterProps = {
 
 function Character({ character }: CharacterProps) {
   return (
-    <article className='flex flex-col justify-center gap-1 w-[600px] md:w-[400px] p-4 cursor-pointer hover:scale-95 transition-all'>
-      <header className='bg-white rounded-lg shadow-lg p-4 h-[260px]'>
-        <img src={character.image} alt={character.name} className='rounded-lg h-full w-full bg-contain' draggable='false' />
-      </header>
-    </article>
+    <Link to={`/personajes/${character.slug}`}>
+      <article className='flex flex-col justify-center gap-1 w-[600px] md:w-[400px] p-4 cursor-pointer hover:scale-95 transition-all'>
+        <header className='bg-white rounded-lg shadow-lg p-4 h-[260px]'>
+          <img src={character.image} alt={character.name} className='rounded-lg h-full w-full bg-contain' draggable='false' />
+        </header>
+      </article>
+    </Link>
   )
 }
 
