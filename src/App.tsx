6f1f5237dc064b770b9fed60ca982e559be766c9
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 13:31:13 -0500

    Add: Simple usage of api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import { getCharacters } from "./services/characters"
+
 function App() {
+  const [data, setData] = useState([])
+  const [state, setState] = useState("idle");
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setState("pending")
+    getCharacters().then((res) => {
+      setData(res)
+      setState("resolved")
+    }).catch((err) => {
+      setError(err)
+      setState("rejected")
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {state === "pending" && <p>Loading...</p>}
+      {state === 'rejected' && <p>{error}</p>}
+      {state === 'resolved' && <p>Resolved</p>}
+      {data && <p>{JSON.stringify(data)}</p>}
     </div>
   )
 }
