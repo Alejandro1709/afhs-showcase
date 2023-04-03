5c1abe9cd667f655b7ff2038335c26d741852f59
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 15:18:06 -0500

    Fix: overflow width

--- src/components/DemoDisplay.tsx
+++ src/components/DemoDisplay.tsx
@@ -2,7 +2,7 @@ import Characters from './Characters'
 
 function DemoDisplay() {
   return (
-    <section className='flex flex-row flex-wrap'>
+    <section className='flex flex-row flex-wrap w-full'>
       <Characters />
     </section>
   )
