4e68449307a648b4cc50397ae9fa6cc457a35442
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 14:28:27 -0500

    Add: Character definition types

---
+++ src/types/character.d.ts
@@ -0,0 +1,19 @@
+type STATUS = 'VIVO' | 'MUERTO' | 'LUCHITO' | 'DESCONOCIDO' | 'DESAPARECIDO';
+
+type GENDER = 'HOMBRE' | 'MUJER';
+
+export default interface ICharacter {
+  id?: string | number;
+  name: string;
+  slug?: string;
+  actor: string | string[];
+  image?: string;
+  birthdate?: string[];
+  work?: string[];
+  otherWork?: string[];
+  status: STATUS;
+  gender: GENDER;
+  height?: number;
+  nicknames?: string[];
+  catchphrases?: string[];
+}
