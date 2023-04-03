99e176e19740f13d10b341b3341dfe136a75ffb0
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 13:33:59 -0500

    Add: Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getCharacters } from "./services/characters"
+import Layout from "./components/Layout";
 
 function App() {
   const [data, setData] = useState([])
@@ -18,12 +19,12 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <Layout>
       {state === "pending" && <p>Loading...</p>}
       {state === 'rejected' && <p>{error}</p>}
       {state === 'resolved' && <p>Resolved</p>}
       {data && <p>{JSON.stringify(data)}</p>}
-    </div>
+    </Layout>
   )
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,9 @@
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="App">
+      {children}
+    </div>
+  )
+}
+
+export default Layout
