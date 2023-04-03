beeff1d00dedaf617fa26610e46ccf31117f18a4
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-03 14:01:20 -0500

    Add: React query

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import { useEffect, useState } from "react"
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getCharacters } from "./services/characters"
 import Layout from "./components/Layout";
 
 function App() {
-  const [data, setData] = useState([])
-  const [state, setState] = useState("idle");
-  const [error, setError] = useState(null)
 
-  useEffect(() => {
-    setState("pending")
-    getCharacters().then((res) => {
-      setData(res)
-      setState("resolved")
-    }).catch((err) => {
-      setError(err)
-      setState("rejected")
-    })
-  }, [])
+  const queryClient = useQueryClient()
+
+  const query = useQuery({ queryKey: ["characters"], queryFn: getCharacters })
 
   return (
     <Layout>
-      {state === "pending" && <p>Loading...</p>}
-      {state === 'rejected' && <p>{error}</p>}
-      {state === 'resolved' && <p>Resolved</p>}
-      {data && <p>{JSON.stringify(data)}</p>}
+      {query.isLoading && <p>Loading...</p>}
+      {query?.data && <p>{JSON.stringify(query?.data)}</p>}
     </Layout>
   )
 }
