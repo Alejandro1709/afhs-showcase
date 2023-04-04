74770ce868f5ced5a96540b5b3e1bbc06e976b00
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-04 12:49:42 -0500

    Add: Controlbar to filter and search characters

---
+++ src/components/ControlBar.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function ControlBar() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className="flex relative items-center gap-2 md:w-2/4 w-[420px] mx-auto mt-4">
+      <input type="text" placeholder="Escribe el nombre..." className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" />
+      <button className="p-2 bg-white hover:bg-gray-200 border rounded-md border-gray-300" onClick={() => setOpen(!open)}>Filtrar</button>
+      <dialog open={open} className="absolute p-2 bg-white border -right-80 md:-right-[95%] top-12 z-20 rounded-md border-gray-300">
+        <div className="flex flex-row gap-2">
+          <input type="checkbox" name="filter" id="filter1" />
+          <label htmlFor="filter1">Vivo</label>
+        </div>
+        <div className="flex flex-row gap-2">
+          <input type="checkbox" name="filter" id="filter2" />
+          <label htmlFor="filter2">Muerto</label>
+        </div>
+        <div className="flex flex-row gap-2">
+          <input type="checkbox" name="filter" id="filter3" />
+          <label htmlFor="filter3">Hombre</label>
+        </div>
+        <div className="flex flex-row gap-2">
+          <input type="checkbox" name="filter" id="filter4" />
+          <label htmlFor="filter4">Mujer</label>
+        </div>
+      </dialog>
+    </div>
+  )
+}
+
+export default ControlBar
